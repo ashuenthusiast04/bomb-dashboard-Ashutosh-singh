b6cdd5087d206d54199bd5c11694245321ced7e6
Ashutosh <ash.chauhansingh@gmail.com>
2023-02-07 01:56:51 +0530

    small changes

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo,useCallback } from 'react';
 import Page from '../../components/Page';
-import { Box, Button,  CardContent, Grid, Paper, Table, Typography } from '@material-ui/core';
+import {useHistory} from 'react-router-dom';
+import { Box, Button,  CardContent, Grid,  Typography } from '@material-ui/core';
 // import { DataGrid } from '@mui/x-data-grid';
 import HomeImage from '../../assets/img/background.jpg';
 import { createGlobalStyle } from 'styled-components';
 import BombImage from '../../assets/img/bomb.png';
 import CountUp from 'react-countup';
+import Footer from '../../components/Footer';
 import useTotalValueLocked from '../../hooks/useTotalValueLocked';
 import useCurrentEpoch from '../../hooks/useCurrentEpoch';
 import ProgressCountdown from '../Boardroom/components/ProgressCountdown';
@@ -29,6 +31,21 @@ import useFetchBoardroomAPR from '../../hooks/useFetchBoardroomAPR';
 import useCashPriceInLastTWAP from '../../hooks/useCashPriceInLastTWAP';
 import Buttoncomp from './Button-comp';
 import Bank from '../../bomb-finance';
+import TVL from '../../bomb-finance/BombFinance';
+import { Token } from 'graphql';
+import ExchangeCard from '../Bond/components/ExchangeCard';
+import useClaimRewardCheck from '../../hooks/boardroom/useClaimRewardCheck';
+import useWithdrawCheck from '../../hooks/boardroom/useWithdrawCheck';
+import useRedeemOnBoardroom from '../../hooks/useRedeemOnBoardroom';
+import useTokenBalance from '../../hooks/useTokenBalance';
+import useBondsPurchasable from '../../hooks/useBondsPurchasable';
+import { useTransactionAdder } from '../../state/transactions/hooks';
+import { BOND_REDEEM_PRICE,BOND_REDEEM_PRICE_BN } from '../../bomb-finance/constants';
+import useApprove,{ApprovalState} from '../../hooks/useApprove';
+import useCashPriceInEstimatedTWAP from '../../hooks/useCashPriceInEstimatedTWAP';
+import bombFinance from '../../bomb-finance';
+import useBombFinance from '../../hooks/useBombFinance';
+
 const BackgroundImage = createGlobalStyle`
   body {
     background: url(${HomeImage}) repeat !important;
@@ -78,7 +95,7 @@ const Card = styled.div`
 // };
 
 const Dashboard = () => {
-  
+  const history= useHistory();
   const TVL = useTotalValueLocked();
   const currentEpoch = useCurrentEpoch();
   const { to } = useTreasuryAllocationTimes();
@@ -86,9 +103,29 @@ const Dashboard = () => {
   const bShareStats = usebShareStats();
   const tBondStats = useBondStats();
   const stakedBalance = useStakedBalanceOnBoardroom();
+  const bombFinance = useBombFinance();
   const totalStaked = useTotalStakedOnBoardroom();
   const cashPrice= useCashPriceInLastTWAP();
-  const scalingFactor = useMemo(() => (cashPrice ? Number(cashPrice) : null), [cashPrice]);
+
+  const cashStat= useCashPriceInEstimatedTWAP();
+  const addTransaction =useTransactionAdder();
+  const bondsPurchasable= useBondsPurchasable();
+  const bondBalance= useTokenBalance(bombFinance?.BBOND);
+  const {onReward} =useHarvest(Bank);
+  const scalingFactor = useMemo(() => (cashStat ? Number(cashStat.priceInDollars).toFixed(4) : null), [cashStat]);
+
+  const isBondRedeemable = useMemo(() => cashPrice.gt(BOND_REDEEM_PRICE_BN), [cashPrice]);
+ 
+  const isBondPurchasable = useMemo(() => Number(tBondStats?.tokenInFtm) < 1.01, [tBondStats]);
+const { onRedeem } = useRedeemOnBoardroom();
+  const canWithdraw = useWithdrawCheck();
+
+  const liveEpochValue = useMemo(() => (cashStat ? Number(cashStat.priceInDollars).toFixed(4) : null), [cashStat]);
+
+  const tokenName = Bank.earnTokenName === 'BSHARE' ? 'BSHARE' : 'BOMB';
+  const canClaimReward= useClaimRewardCheck();
+
+   
   // const earnings = useEarnings(Bank.contract, Bank.earnTokenName, Bank.poolId);
   // const tokenName = Bank.earnTokenName === 'BSHARE' ? 'BSHARE' : 'BOMB';
   //   const tokenStats = Bank.earnTokenName === 'BSHARE' ? bShareStats : bombStats;
@@ -130,6 +167,28 @@ const Dashboard = () => {
   );
   const bombPriceInBNB = useMemo(() => (bombStats ? Number(bombStats.tokenInFtm).toFixed(4) : null), [bombStats]);
   const tBondPriceInBNB = useMemo(() => (tBondStats ? Number(tBondStats.tokenInFtm).toFixed(4) : null), [tBondStats]);
+  const handleBuyBonds = useCallback(
+    async (amount) => {
+      const tx = await bombFinance?.buyBonds(amount);
+      addTransaction(tx, {
+        summary: `Buy ${Number(amount).toFixed(2)} BBOND with ${amount} BOMB`,
+      });
+    },
+    [bombFinance, addTransaction],
+  );
+  const [approveStatus, approve] = useApprove(bombFinance.BSHARE, bombFinance.contracts.Boardroom.address);
+
+  const handleRedeemBonds = useCallback(
+    async (amount) => {
+      const tx = await bombFinance?.redeemBonds(amount);
+      addTransaction(tx, { summary: `Redeem ${amount} BBOND` });
+    },
+    [bombFinance, addTransaction],
+  );
+  // let lastEpochValue = 0;
+  // useEffect(() => {
+  //   lastEpochValue = liveEpochValue;
+  // }, [currentEpoch]);
 return(
 <Page>
 <BackgroundImage />
@@ -192,7 +251,7 @@ return(
     <h1>  <ProgressCountdown base={moment().toDate()} hideBar={true} deadline={to} description="Next Epoch" /></h1>
     <h3>Next EPOCH in</h3>
     <hr  style={{opacity:'0.2'}} />
-    <h5>Live TWAP: 1.17</h5>
+    <h5>Live TWAP: {scalingFactor}</h5>
     <h5>TVL: <CountUp style={{ fontSize: '25px' }} end={TVL} separator="," prefix="$" /></h5>
     <h5>Last Epoch TWAP: 1.22
     </h5>
@@ -206,24 +265,24 @@ return(
         
        
                 <Grid container rowSpacing={1}>
-                <Card style={{ float: 'right', width: '20%', marginRight: '2rem', marginLeft: '2px', height: '23rem' }}>
+                {/* <Card style={{ float: 'right', width: '20%', marginRight: '2rem', marginLeft: '2px', height: '23rem' }}>
           <h2>Latest News</h2>
-        </Card>
+        </Card> */}
                 <Grid item xs={8}>
-                <span style={{marginLeft:'5rem'}}>
-        <Button className='button'style={{width:'100%'}}>
+                <span style={{marginLeft:'0rem',color:'rgba(0,245,171,0.5)'}}>
+        <Button className='button'style={{width:'98%'}}>
                   Invest Now
                 </Button>
                
-                <Button className='button' style={{width:'42%',marginTop:'30px',marginRight:'5px',marginLeft:'6rem'}}>
+                <Button className='button' style={{width:'48%',marginTop:'30px',marginLeft:'8px'}}>
                   Chat On Discord
                 </Button>
-                <Button className='button' style={{width:'44%',marginTop:'30px'}}>
+                <Button className='button' style={{width:'49%',marginTop:'30px'}}>
                   Read Docs
                 </Button></span>
                 
                
-          <Card style={{marginLeft:'5rem'}}>
+          <Card style={{marginLeft:'0rem'}}>
             
           <img src={BombImage} alt="Bomb.money" style={{ maxHeight: '40px',float:'left',padding:'7px' }} />
           <div className='header'>
@@ -258,15 +317,62 @@ return(
               </span>
   </Grid>
   <Grid item xs={4}>
- <Button className='button1' style={{width:'100px',marginTop:'0px',marginRight:'5px'}}>
-                  Deposit
-                </Button>
-                <Button className='button1' style={{width:'100px',marginTop:'0px'}}>
-                  Withdraw
-                </Button>
-                 <Button className='button1' style={{width:'200px',marginTop:'10px'}}>
-                  Claim Rewards 
-                </Button>
+  <div
+                style={{
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  borderRadius: '20px',
+                  margin: '5px',
+                }}
+                disabled={approveStatus !== ApprovalState.NOT_APPROVED}
+                onClick={approve}
+              >
+                Deposit
+                &emsp;
+                    <TokenSymbol symbol="UP" size={16} />
+              </div>
+              <div
+                style={{
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  borderRadius: '20px',
+                  margin: '5px',
+                }}
+                disabled={stakedBalance.eq(0) || (!canWithdraw && !canClaimReward)}
+                onClick={onRedeem}
+              >
+                Withdraw
+                &emsp;
+                    <TokenSymbol symbol="DOWN" size={15} />
+              
+              </div>
+              <div
+                style={{
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  borderRadius: '20px',
+                  margin: '5px',
+                }}
+                onClick={onReward}
+                disabled={earnings.eq(0) || !canClaimReward}
+              >
+                Claim Reward
+                &emsp;
+                    <TokenSymbol symbol="BSHARE" size={16} />
+              
+              </div>
   </Grid>
 </Grid>
            </div>
@@ -276,7 +382,9 @@ return(
 
        
          </Grid>
-       
+         <Card style={{ float: 'right', width: '20%', marginLeft: '120px', height: '23rem' }}>
+          <h2>Latest News</h2>
+        </Card>
          
         </Grid>
         
@@ -316,7 +424,7 @@ return(
             </span>
           </div>
           <div style={{ fontSize: '20px', display: 'flex', alignContent: 'center', alignItems: 'center' }}>
-            TVL:$<CountUp style={{ fontSize: '25px' }} end={TVL} separator="," prefix="$" />
+            TVL:<CountUp style={{ fontSize: '25px' }} end={TVL} separator="," prefix="$" />
           </div>
         </div>
         <hr />
@@ -364,11 +472,66 @@ return(
 
           <div style={{display:"flex", alignItems:"flex-end",justifyContent:'space-between'}}>
             {/* href={buyBombAddress} */}
-            <Buttoncomp text="Deposit" symbol="UP" />
+            <div
+                style={{
+                  width:'100px',
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  borderRadius: '20px',
+                  marginBottom: '5px',
+                }}
+                disabled={approveStatus !== ApprovalState.NOT_APPROVED}
+                onClick={approve}
+              >
+                Deposit
+                &emsp;
+                    <TokenSymbol symbol="UP" size={12} />
+              </div>
               {/* href={buyBombAddress} */}
-              <Buttoncomp text="Withdraw" symbol="DOWN" />
+              <div
+                style={{
+                  width:'120px',
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  margin: '5px',
+                  borderRadius: '20px',
+                }}
+                disabled={stakedBalance.eq(0) || (!canWithdraw && !canClaimReward)}
+                onClick={onRedeem}
+              >
+                Withdraw
+                &emsp;
+                    <TokenSymbol symbol="DOWN" size={12} />
+              
+              </div>
               {/* href={buyBombAddress} */}
-              <Buttoncomp text="Claim Rewards" symbol="BSHARE" />
+              <div
+                style={{
+                  width:'150px',
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  margin: '5px',
+                  borderRadius: '20px',
+                }}
+                onClick={onReward}
+                disabled={earnings.eq(0) || !canClaimReward}
+              >
+                Claim Reward&emsp;
+                    <TokenSymbol symbol="BSHARE" size={16} />
+              
+              </div>
           </div>
         </div>
         <hr style={{border:"1px solid rgba(0, 173, 232, 1)"}}/>
@@ -392,7 +555,7 @@ return(
             </span>
           </div>
           <div style={{ fontSize: '20px', display: 'flex', alignContent: 'center', alignItems: 'center' }}>
-            TVL:$<CountUp style={{ fontSize: '25px' }} end={TVL} separator="," prefix="$" />
+            TVL:<CountUp style={{ fontSize: '25px' }} end={TVL} separator="," prefix="$" />
           </div>
         </div>
         <hr />
@@ -440,12 +603,66 @@ return(
   </Grid>
 
           <div style={{display:"flex", alignItems:"flex-end"}}>
-            {/* href={buyBombAddress} */}
-            <Buttoncomp text="Deposit" symbol="UP" />
+          <div
+                style={{
+                  width:'100px',
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  borderRadius: '20px',
+                  marginBottom: '5px',
+                }}
+                disabled={approveStatus !== ApprovalState.NOT_APPROVED}
+                onClick={approve}
+              >
+                Deposit
+                &emsp;
+                    <TokenSymbol symbol="UP" size={12} />
+              </div>
               {/* href={buyBombAddress} */}
-              <Buttoncomp text="Withdraw" symbol="DOWN" />
+              <div
+                style={{
+                  width:'120px',
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  margin: '5px',
+                  borderRadius: '20px',
+                }}
+                disabled={stakedBalance.eq(0) || (!canWithdraw && !canClaimReward)}
+                onClick={onRedeem}
+              >
+                Withdraw
+                &emsp;
+                    <TokenSymbol symbol="DOWN" size={12} />
+              
+              </div>
               {/* href={buyBombAddress} */}
-              <Buttoncomp text="Claim Rewards" symbol="BSHARE" />
+              <div
+                style={{
+                  width:'150px',
+                  height: '20px',
+                  color: 'white',
+                  backgroundColor: 'transparent',
+                  cursor: 'pointer',
+                  border: '1px solid white',
+                  padding: '5px 8px 5px 10px',
+                  margin: '5px',
+                  borderRadius: '20px',
+                }}
+                onClick={onReward}
+                disabled={earnings.eq(0) || !canClaimReward}
+              >
+                Claim Reward&emsp;
+                    <TokenSymbol symbol="BSHARE" size={16} />
+              
+              </div>
           </div>
         </div>
 
@@ -486,14 +703,47 @@ return(
   Available to redeem:  <br />
   456   </span>
   <span>
-   <Button className='button2' style={{width:'100px',marginTop:'-10px',marginLeft:'200px'}}>
+  <div
+                  style={{
+                    width:'120px',
+                    height: '20px',
+                    color: 'white',
+                    backgroundColor: 'transparent',
+                    cursor: 'pointer',
+                    border: '1px solid white',
+                    padding: '5px 8px 5px 10px',
+                    borderRadius: '20px',
+                  }}
+                  onClick={handleBuyBonds}
+                  disabled={!tBondStats || isBondRedeemable}
+                >
                   Purchase
-                </Button>
+                  &emsp;
+                    <TokenSymbol symbol="PURCHASE" size={16} />
+
+                </div>
           <hr />
           Redeem Bomb
-          <Button className='button2' style={{width:'100px',marginTop:'-10px',marginLeft:'240px'}}>
+          <div
+                  style={{
+                    width:'120px',
+
+                    height: '20px',
+                    color: 'white',
+                    backgroundColor: 'transparent',
+                    cursor: 'pointer',
+                    border: '1px solid white',
+                    padding: '5px 8px 5px 10px',
+                    borderRadius: '20px',
+                  }}
+                  onClick={handleRedeemBonds}
+                  disabled={!tBondStats || bondBalance.eq(0) || !isBondRedeemable}
+                >
                   Redeem
-                </Button>
+                  &emsp;
+                    <TokenSymbol symbol="DOWN" size={16} />
+              
+                </div>
               </span>
  {/* <Button className='button1' style={{width:'100px',marginTop:'0px',marginRight:'5px'}}>
                   Deposit
@@ -510,6 +760,7 @@ return(
           
         </Card>
       </Grid>
+      {/* <Footer /> */}
 </Page>
 
 );
